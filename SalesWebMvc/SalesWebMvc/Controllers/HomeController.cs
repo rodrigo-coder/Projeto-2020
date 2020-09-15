405c489a4c22eb9d54a680bd3ee0af78758c7cfc
Rodrigo Prestes <rodrigoprestes3@yahoo.com.br>
2020-09-15 20:54:34 -0300

    First controller and Razor pages tests

--- SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web Mvc from C#.";
+            ViewData["Professor"] = "Rodrigo";
             return View();
         }
 
